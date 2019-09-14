a09f595c5d7c57d424adf8509b177d150d800e88
Megamage1 <radexilic@gmail.com>
2019-09-14 14:06:09 +0200

    new java file added

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String[] monatsnamen = {"Jan", "Feb", "Mar"};
+	    int length = monatsnamen.length;
+        for (int i = 0; i < length; i++) {
+            System.out.println(monatsnamen[i]);
+            gay
+        }
+    }
+}
